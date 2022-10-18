d0f5f20a4bd1cf2ef8284aca4aba4e331c34260b
chanaiagwata <chanaiagwata82@gmil.com>
2022-10-18 23:52:44 +0300

    adds description to goals

---
+++ src/app/goal-detail-component/goal-detail-component.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GoalDetailComponentComponent } from './goal-detail-component.component';
+
+describe('GoalDetailComponentComponent', () => {
+  let component: GoalDetailComponentComponent;
+  let fixture: ComponentFixture<GoalDetailComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GoalDetailComponentComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GoalDetailComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/goal-detail-component/goal-detail-component.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Goal } from '../goal';
+@Component({
+  selector: 'app-goal-detail-component',
+  templateUrl: './goal-detail-component.component.html',
+  styleUrls: ['./goal-detail-component.component.css']
+})
+export class GoalDetailComponentComponent implements OnInit {
+
+  @Input() goal!: Goal;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
