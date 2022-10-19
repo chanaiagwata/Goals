c0a37ea6908875ac858679208609d8ce9cab75eb
chanaiagwata <chanaiagwata82@gmil.com>
2022-10-19 15:29:51 +0300

    adds strikethrough directive and date pipe

--- src/app/goal.ts
+++ src/app/goal.ts
@@ -1,11 +1,13 @@
 export class Goal {
 
     showDescription: boolean;
-    constructor(public id:number, public name:string, public description:string){
+
+    constructor(public id:number, public name:string, public description:string, public completeDate:Date){
     //    The public keyword is an access modifier and it determines where the class properties are visible which in our case is anywhere outside the class.
         this.id = id;
         this.name = name;
         this.description = description;
         this.showDescription = false;
+
     }
   }
